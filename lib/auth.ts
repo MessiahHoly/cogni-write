fe1c6177dd5c8dda089502fa13d1a254b35ff4ad
Masaya Hori <masaya.hori@gmail.com>
2026-06-17 17:07:53 +0900

    Set up resend for better auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,44 @@ import { betterAuth } from "better-auth/minimal";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 // import { PrismaClient } from "@/generated/prisma/client";
-import { prisma } from "../data/prisma";
+import { prisma } from "./data/prisma";
+import { magicLink } from "better-auth/plugins";
+import { Resend } from "resend";
 
 // const prisma = new PrismaClient();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "postgresql", 
+    provider: "postgresql",
   }),
+  plugins: [
+    magicLink({
+      sendMagicLink: async ({ email, token, url, metadata }) => {
+        const from = process.env.RESEND_FROM_EMAIL;
+        if (!from) {
+          console.error("RESEND_FROM_EMAIL is not set in environment variables.");
+          return
+        };
+
+        try {
+          const { data, error } = await resend.emails.send({
+            from,
+            to: email,
+            subject: "Your Magic Link",
+            html: `<p>Click the link below to log in:</p><p><a href="${url}">Log In</a></p>`
+          })
+
+          if (error) {
+            console.error("Error sending email:", error);
+            return
+          };          
+        } catch (error) {
+          console.error("Error sending email:", error);
+          return
+        }
+      },
+    }),
+  ],
 });

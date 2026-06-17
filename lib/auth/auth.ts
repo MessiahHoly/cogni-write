333fc473de690a61ae9c4f394167821d10a00a27
Masaya Hori <masaya.hori@gmail.com>
2026-06-18 07:15:24 +0900

    Move auth related files to auth folder

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth/minimal";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
 // import { PrismaClient } from "@/generated/prisma/client";
-import { prisma } from "./data/prisma";
+import { prisma } from "../data/prisma";
 import { magicLink } from "better-auth/plugins";
 import { Resend } from "resend";
 

025a9d7a5d98e659c78f7429c664a0ed3729690a
Masaya Hori <masaya.hori@gmail.com>
2026-06-18 05:38:10 +0900

    Set up Tailwind CSS Typography

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <main className="p-4">
+    <main className="prose p-4">
       <h1>Admin Page</h1>
       <p>Welcome to the admin dashboard.</p>
     </main>

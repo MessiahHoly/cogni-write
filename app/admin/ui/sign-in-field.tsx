f440a4e216ec5f195f2b4fa57925c0f3aba0247f
Masaya Hori <masaya.hori@gmail.com>
2026-06-19 06:19:18 +0900

    Send magic link

--- app/admin/ui/sign-in-field.tsx
+++ app/admin/ui/sign-in-field.tsx
@@ -1,25 +1,48 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import {
   Field,
-  // FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldLegend,
-  // FieldSeparator,
   FieldSet,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth/auth-client"
+import { SubmitEvent, useState } from "react"
 
 export function SignInField() {
+  const [email, setEmail] = useState("")
+  const [magicLinkSent, setMagicLinkSent] = useState(false)
+
+  const handleSubmit = async (event: SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    // Handle form submission logic here
+    const { data, error } = await authClient.signIn.magicLink({
+      email,
+    })
+    if (data) {
+      setMagicLinkSent(true)
+    }
+  }
+
+  if (magicLinkSent) {
+    return (
+      <div className="w-full max-w-md">
+        <p className="text-sm text-muted-foreground">
+          A magic link has been sent to your email. Please check your inbox.
+        </p>
+      </div>
+    )
+  }
+
   return (
     <div className="w-full max-w-md">
-      <form>
+      <form onSubmit={handleSubmit}>
         <FieldGroup>
           <FieldSet>
             <FieldLegend>Sign in</FieldLegend>
-            {/* <FieldDescription>
-              All transactions are secure and encrypted
-            </FieldDescription> */}
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">
@@ -29,67 +52,16 @@ export function SignInField() {
                   id="email"
                   placeholder="you@example.com"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
                 />
               </Field>
-              {/* <Field>
-                <FieldLabel htmlFor="checkout-7j9-card-number-uw1">
-                  Card Number
-                </FieldLabel>
-                <Input
-                  id="checkout-7j9-card-number-uw1"
-                  placeholder="1234 5678 9012 3456"
-                  required
-                />
-                <FieldDescription>
-                  Enter your 16-digit card number
-                </FieldDescription>
-              </Field> */}
-              {/* <div className="grid grid-cols-3 gap-4">
-                <Field>
-                  <FieldLabel htmlFor="checkout-exp-month-ts6">
-                    Month
-                  </FieldLabel>
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="checkout-7j9-exp-year-f59">
-                    Year
-                  </FieldLabel>               
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="checkout-7j9-cvv">CVV</FieldLabel>
-                  <Input id="checkout-7j9-cvv" placeholder="123" required />
-                </Field>
-              </div> */}
             </FieldGroup>
           </FieldSet>
-          {/* <FieldSeparator /> */}
-          {/* <FieldSet>
-            <FieldLegend>Billing Address</FieldLegend>
-            <FieldDescription>
-              The billing address associated with your payment method
-            </FieldDescription>
-            <FieldGroup>
-              <Field orientation="horizontal">
-                <FieldLabel
-                  htmlFor="checkout-7j9-same-as-shipping-wgm"
-                  className="font-normal"
-                >
-                  Same as shipping address
-                </FieldLabel>
-              </Field>
-            </FieldGroup>
-          </FieldSet> */}
-          {/* <FieldSet>
-            <FieldGroup>
-              <Field>
-                <FieldLabel htmlFor="checkout-7j9-optional-comments">
-                  Comments
-                </FieldLabel>
-              </Field>
-            </FieldGroup>
-          </FieldSet> */}
           <Field orientation="horizontal">
             <Button type="submit">Sign in</Button>
+            {/* <Button type="submit">Sign in</Button> */}
             <Button variant="outline" type="button">
               Cancel
             </Button>

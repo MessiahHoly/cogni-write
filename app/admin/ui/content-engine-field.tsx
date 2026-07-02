36622aaecf036a2a7a698b03fae750af669ceacb
Masaya Hori <masaya.hori@gmail.com>
2026-07-03 06:33:16 +0900

    Update nextjs

--- app/admin/ui/content-engine-field.tsx
+++ app/admin/ui/content-engine-field.tsx
@@ -8,8 +8,6 @@ import { useActionState } from "react";
 import ButtonField from "./button-field";
 import { createContentEngine } from "@/lib/actions/content-engine";
 
-//TODO: update nextjs
-
 export default function ContentEngineField({ contentEngine, onSuccess }: { contentEngine: ContentEngine | null; onSuccess: () => void }) {
   const [state, action, pending] = useActionState(createContentEngine, null)
 

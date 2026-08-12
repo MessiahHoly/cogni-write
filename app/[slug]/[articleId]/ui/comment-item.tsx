a4ad34546ba99c4599e0a883588ff9d09565d497
Masaya Hori <masaya.hori@gmail.com>
2026-08-13 08:42:55 +0900

    Update comment item so CommentItem is nested for nested comments

--- app/[slug]/[articleId]/ui/comment-item.tsx
+++ app/[slug]/[articleId]/ui/comment-item.tsx
@@ -5,13 +5,15 @@ import { Prisma } from "@/generated/prisma/client";
 import { MessageSquare } from "lucide-react";
 import { useState } from "react";
 import CommentField from "./comment-field";
+import { CommentNode } from "@/lib/schemas/comment";
 
-export default function CommentItem({ comment, isAuthenticated }: {
-  comment: Prisma.CommentGetPayload<{
-    include: { comments: { include: { user: { select: { name: true } } } }, user: { select: { name: true } } }
-  }>,
-  isAuthenticated: boolean
-}) {
+export default function CommentItem({ comment, isAuthenticated }: { comment: CommentNode, isAuthenticated: boolean }) {
+  // export default function CommentItem({ comment, isAuthenticated }: {
+  //   comment: Prisma.CommentGetPayload<{
+  //     include: { comments: { include: { user: { select: { name: true } } } }, user: { select: { name: true } } }
+  //   }>,
+  //   isAuthenticated: boolean
+  // }) {
   const [isReplying, setIsReplying] = useState(false);
 
   return (
@@ -30,7 +32,9 @@ export default function CommentItem({ comment, isAuthenticated }: {
             })}
           </span>
         </div>
+
         <p className="text-foreground/90 text-sm whitespace-pre-wrap">{comment.content}</p>
+
         {isAuthenticated && (
           <Button
             variant="ghost"
@@ -60,7 +64,10 @@ export default function CommentItem({ comment, isAuthenticated }: {
       {/* Render Nested Replies */}
       {comment.comments.length > 0 && (
         <div className="ml-6 pl-4 border-l-2 border-muted space-y-4 pt-2">
-          {comment.comments.map((reply) => (
+          {comment.comments.map(childComment=>(
+            <CommentItem key={childComment.id} comment={childComment} isAuthenticated={isAuthenticated} />
+          ))}
+          {/* {comment.comments.map((reply) => (
             <div key={reply.id} className="space-y-1 text-sm">
               <div className="flex items-center gap-2">
                 <span className="font-semibold">{reply.user.name}</span>
@@ -76,7 +83,7 @@ export default function CommentItem({ comment, isAuthenticated }: {
               </div>
               <p className="text-foreground/90 whitespace-pre-wrap">{reply.content}</p>
             </div>
-          ))}
+          ))} */}
         </div>
       )}
     </div>

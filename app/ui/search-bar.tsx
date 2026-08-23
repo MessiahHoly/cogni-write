f0225197c23c817dc5bd23537acec1f5a82acaa1
Masaya Hori <masaya.hori@gmail.com>
2026-08-23 16:18:58 +0900

    Create SearchBar UI commonet

---
+++ app/ui/search-bar.tsx
@@ -0,0 +1,33 @@
+import { Input } from "@/components/ui/input";
+import { Loader2, Search } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useTransition } from "react";
+
+export default function SearchBar() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [isPending, startTransition] = useTransition();
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("q", term);
+    } else {
+      params.delete("q");
+    }
+
+    startTransition(() => {
+      router.push(`${pathname}?${params.toString()}`);
+    });
+  }
+
+  return (
+    <div className="relative w-full">
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+      <Input type="search" defaultValue={searchParams.get("q")?.toString()} onChange={e => handleSearch(e.target.value)}
+        placeholder="Search articles and comments..." className="pl-9 pr-9" />
+      {isPending && <Loader2 className="absolute right-3 top-1/2 -translate-y-1/2 h-4 w-4 animate-spin text-muted-foreground" />}
+    </div>
+  )
+}

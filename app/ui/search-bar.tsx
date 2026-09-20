e18250e438ebae016a23dfe60fa3d249a2b318db
Masaya Hori <masaya.hori@gmail.com>
2026-09-21 08:40:02 +0900

    Remove comment

--- app/ui/search-bar.tsx
+++ app/ui/search-bar.tsx
@@ -36,8 +36,6 @@ const SearchBarContent = () => {
     });
   }, 300);
 
-  //TODO: npm audit fix
-
   return (
     <div className="relative w-full">
       <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
